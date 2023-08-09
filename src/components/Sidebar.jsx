e41e28e55f87a73977a0529567b8d1c46a714206
abdul rehman <abdulrehmanmangral86@gmail.com>
2023-08-09 23:02:23 +0500

    managing app layout

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import AppNav from "./AppNav";
+import styles from "./Sidebar.module.css";
+import Logo from "./Logo";
+import Footer from "./Footer";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <p>List of cities</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default Sidebar;
